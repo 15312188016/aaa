16767120f910bb2b3ddec4ef136cf9bda9e0a68d
15312188016 <59191144+15312188016@users.noreply.github.com>
2021-10-29 14:08:47 +0800

    Delete v3finance directory

--- v3finance/config/conf.py
+++
@@ -1,2 +0,0 @@
-url_dev = "https://taidiiv3.taidii.cn"
-url_v3 = "https://v3.taidii.cn"
--- v3finance/connect_sql/sql_config.py
+++
@@ -1,5 +0,0 @@
-host_taidii="121.196.212.68"
-user_taidii="taidii"
-password_taidii="taidiitest"
-port_taidii=3306
-db_taidii="taidiiv3"
